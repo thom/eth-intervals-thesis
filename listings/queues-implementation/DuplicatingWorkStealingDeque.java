1017bd974cb2faf2be70dc576864fa16197cf6cd
Thomas Weibel <thom@beeblebrox.net>
2010-08-09 15:47:51 +0200

    Remove unneeded listings

--- listings/queues-implementation/DuplicatingWorkStealingDeque.java
+++
@@ -1,9 +0,0 @@
-public class DuplicatingWorkStealingDeque 
-		implements WorkStealingQueue {
-	private int size = 1024;
-	private WorkItem[] workItems = new WorkItem[size];
-	private int tailMin = Integer.MAX_VALUE;
-	private volatile int tail = 0;
-	private volatile int head = 0;
-	// ...
-}
