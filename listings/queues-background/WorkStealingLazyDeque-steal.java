ffc5e1adf92b9e6ba6bc0989301267ac9838b52b
Thomas Weibel <thom@beeblebrox.net>
2010-08-21 09:28:43 +0200

    More ch-ch-ch-changes

--- listings/queues-background/WorkStealingLazyDeque-steal.java
+++ listings/queues-background/WorkStealingLazyDeque-steal.java
@@ -1,15 +1,18 @@
 public WorkItem steal() {
-	synchronized (thief) {
+	synchronized (thief) { //*\label{lst:work-stealing-lazy-deque-steal-sync-1}
 		final int head = thief.head;
 		final int index = head % workItems.length;
 		WorkItem workItem = workItems.get(index);
-		if (!workItems.compareAndSet(index, workItem, null))
-			workItem = null;
 
-		if (workItem == null)
-			return null;
+		if (!workItems.compareAndSet(index, workItem, null)) {
+			workItem = null;
+		}
+
+		if (workItem == null) {
+			return null; //*\label{lst:work-stealing-lazy-deque-steal-null}
+		}
 
 		thief.head++;
-		return workItem;
-	}
+		return workItem; //*\label{lst:work-stealing-lazy-deque-steal-item}
+	} //*\label{lst:work-stealing-lazy-deque-steal-sync-2}
 }
